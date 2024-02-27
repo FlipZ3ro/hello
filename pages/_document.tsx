a53ff1352b85e369cb77eacfdcf32c376cbe4006
FlipZ3ro <tokenla15@gmail.com>
2024-02-28 06:34:22 +0700

    update pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,10 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          {/* Tidak perlu menyertakan link stylesheet di sini */}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
-          {/* Tambahkan link stylesheet di sini menggunakan Document */}
-          <link rel="stylesheet" href="/style.css" />
         </body>
       </Html>
     );
